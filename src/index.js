12ebb79be60bc72c9ce36223e4263cf05dd645ea
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-06-02 14:53:17 +0100

    rendered articles

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 /*
+RENDERING FEED
 1. ✔ Get the feed articles
 2. ✔ Put feed articles into state
 3. ✔ Create feed article
 4. ✔ Create feed articles
 5. ✔ Render feed articles to page
-6. Move to filtering
+*/
+/*
+FILTERING
+Search:
+1. Have an event listener on theb submit of the search input
+2. Filter the publishesd articles with the search string and add to state 
+3. Render the filtered articles to the page
 */
 function createEl(tag) {
     return document.createElement(tag)
 }
 let filterFeedSection = document.querySelector(".filterFeed")
 let mainContentSection = document.querySelector(".mainContent")
+let searchInputEl = document.querySelector(".searchbar")
+
+function listenForSearchBar() {
+    searchInputEl.addEventListener("submit",function(e) {
+        e.preventDefault()
+
+        state.filters.search = searchInputEl.value
+        renderArticles()
+    })
+}
+listenForSearchBar()
 
 
 let state = {
     toBePushlished: [],
-    published: []
+    published: [],
+    filters: {
+        search: "",
+        pillars: []
+    }
 }
 function setState(keyToUpdate) {
     state = {...state, keyToUpdate}
@@ -74,11 +96,21 @@ function renderFeedArticle(article) {
 function renderArticles() {
     const feedContainerEl = createEl("div")
     feedContainerEl.setAttribute("class", "feedContainer")
+
+    let articlesToBeFiltered = state.published
     
-    for (article of state.published) {
-        articleHTML = renderFeedArticle(article)
-        feedContainerEl.append(articleHTML)
+    if(state.filters.search === "") {
+        for (article of articlesToBeFiltered) {
+            articleHTML = renderFeedArticle(article)
+            feedContainerEl.append(articleHTML)
+        }
     }
+    if(state.filters.search !==  "") {
+        let filteredArticles = articlesToBeFiltered.filter(function(article) {
+            return article.title.includes(state.filters.search) || article.content.includes(state.filters.search)
+        })
+    }
+        
 
     mainContentSection.append(feedContainerEl)
 }
